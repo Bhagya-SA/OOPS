7e590fff43fad497eb8098d6b989a62ec2c2d7b0
Bhagyashree S A <184914610+Bhagya-SA@users.noreply.github.com>
2025-11-19 16:39:28 +0530

    Add classes A and B with constructor messages
    Implement classes A and B with constructors.

---
+++ k)Final3.java
@@ -0,0 +1,18 @@
+final class A {
+    A() {
+        System.out.println("A's constructor");
+    }
+}
+class B extends A {
+    B() {
+        System.out.println("B's constructor");
+    }
+}
+
+
+public class Final3 {
+    public static void main (String[] args) {
+
+    }
+    
+}
