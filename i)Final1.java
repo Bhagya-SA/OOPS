710dbe9d37fa237d83b06b84e0c16f39b40c96bf
Bhagyashree S A <184914610+Bhagya-SA@users.noreply.github.com>
2025-11-19 16:35:30 +0530

    Create Final1.java with final variable example

---
+++ i)Final1.java
@@ -0,0 +1,9 @@
+public class Final1 {
+    public static void main (String[] args) {
+        final int a = 100;
+        System.out.println("a's value = " + a);
+    //    a = 10000; can't change the value of variable which is final
+    //     System.out.println("a's value = " + a);
+    }
+    
+}
